33fa1cf3992f945e1d4548f4ecdd524666489104
Jerin Philip <jerinphilip@live.in>
2022-04-06 15:38:43 +0100

    Update benchmark code for Oracle Cloud (ARM) pull

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <random>
 #include "3rd-party/CLI11.hpp"
 #include "gemm.h"
 #include "tensor.h"
@@ -65,7 +66,11 @@ void dispatch(std::string provider,
 
 int main(int argc, char *argv[]) {
   size_t batchSize = 1;
-  size_t M = 2000, K = 1000, N = 3000;
+  std::mt19937_64 gen64;
+
+  size_t maxM = 128, maxK = 128, maxN = 128;
+  size_t minM = 64, minK = 64, minN = 64;
+  int seed = 42;
 
   float alpha       = 3.0;
   float beta        = 1.0;
@@ -77,14 +82,17 @@ int main(int argc, char *argv[]) {
 
   // clang-format off
   app.add_option("--batchSize"  , batchSize  , "Batches to pack together");
-  app.add_option("--rowsA"      , M          , "Rows of A");
-  app.add_option("--colsA"      , K          , "Cols of A = rows B");
-  app.add_option("--colsB"      , N          , "Cols of B");
+  app.add_option("--rowsA"      , maxM       , "Rows of A, must be greater than 64");
+  app.add_option("--colsA"      , maxK       , "Cols of A = rows B, must be greater than 64");
+  app.add_option("--colsB"      , maxN       , "Cols of B, must be greater than 64");
   app.add_option("--provider"   , provider   , "SGGEMM provider");
   app.add_option("--iterations" , iterations , "Number of iterations");
+  app.add_option("--seed"       , seed       , "Seed for random init");
   // clang-format on
 
   CLI11_PARSE(app, argc, argv);
+
+  gen64.seed(seed);
 
   marian::Tensor A, B;
   for(size_t i = 0; i < iterations; i++) {
@@ -92,6 +100,12 @@ int main(int argc, char *argv[]) {
     for(const bool transA : transU) {
       for(const bool transB : transU) {
         marian::Tensor A, B;
+
+        size_t M, N, K;
+        // std::cout << M << "x" << K << ", " << K << "x" << N << "\n";
+        M = minM + gen64() % (maxM - minM + 1);
+        N = minN + gen64() % (maxN - minM + 1);
+        K = minN + gen64() % (maxK - minM + 1);
 
         auto generate = [batchSize](size_t rows, size_t cols) {
           return marian::make_tensor({batchSize, rows, cols});
