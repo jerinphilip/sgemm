eae2e269b58f77290d5d30b3f58a01c079609c6f
Jerin Philip <jerinphilip@live.in>
2022-04-06 20:18:13 +0100

    Oh wait this can be a function

--- src/tests.cpp
+++ src/tests.cpp
@@ -59,40 +59,41 @@ TEST(RuyVsBLAS, Batched) {
   Batched(100);
 }
 
-#define COMPARE_RANDOM(Provider1, Provider2)                      \
-  const size_t m = 20, k = 10, n = 30, batchSize = 2;             \
-  const float alpha = 2.0, beta = 3.0;                            \
-                                                                  \
-  std::vector<bool> transU = {true, false};                       \
-  for(const bool transA : transU) {                               \
-    for(const bool transB : transU) {                             \
-      marian::Tensor A, B;                                        \
-                                                                  \
-      auto generate = [batchSize](size_t rows, size_t cols) {     \
-        return marian::make_tensor({batchSize, rows, cols});      \
-      };                                                          \
-                                                                  \
-      A = transA ? generate(k, m) : generate(m, k);               \
-      B = transB ? generate(n, k) : generate(k, n);               \
-                                                                  \
-      auto C1 = marian::make_tensor({batchSize, m, n});           \
-                                                                  \
-      dispatch(Provider1, C1, A, B, transA, transB, beta, alpha); \
-      SGEMM_DEBUG(C1);                                            \
-                                                                  \
-      auto C2 = marian::make_tensor({batchSize, m, n});           \
-      dispatch(Provider2, C2, A, B, transA, transB, beta, alpha); \
-      SGEMM_DEBUG(C2);                                            \
-                                                                  \
-      bool close = marian::is_close(C1, C2);                      \
-      ASSERT_EQ(close, true);                                     \
-    }                                                             \
+void compare_random(std::string Provider1, std::string Provider2) {
+  const size_t m = 20, k = 10, n = 30, batchSize = 2;
+  const float alpha = 2.0, beta = 3.0;
+
+  std::vector<bool> transU = {true, false};
+  for(const bool transA : transU) {
+    for(const bool transB : transU) {
+      marian::Tensor A, B;
+
+      auto generate = [batchSize](size_t rows, size_t cols) {
+        return marian::make_tensor({batchSize, rows, cols});
+      };
+
+      A = transA ? generate(k, m) : generate(m, k);
+      B = transB ? generate(n, k) : generate(k, n);
+
+      auto C1 = marian::make_tensor({batchSize, m, n});
+
+      dispatch(Provider1, C1, A, B, transA, transB, beta, alpha);
+      SGEMM_DEBUG(C1);
+
+      auto C2 = marian::make_tensor({batchSize, m, n});
+      dispatch(Provider2, C2, A, B, transA, transB, beta, alpha);
+      SGEMM_DEBUG(C2);
+
+      bool close = marian::is_close(C1, C2);
+      ASSERT_EQ(close, true);
+    }
   }
+}
 
 TEST(RuyVsMKL, Combinations) {
-  COMPARE_RANDOM("ruy", "mkl");
+  compare_random("ruy", "mkl");
 }
 
 TEST(RuyVsEigen, Combinations) {
-  COMPARE_RANDOM("ruy", "eigen");
+  compare_random("ruy", "eigen");
 }
