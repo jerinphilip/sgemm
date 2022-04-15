aa1debe774a95f26f49d083e5cabd840230519f4
Jerin Philip <jphilip@ed.ac.uk>
2022-04-15 13:34:12 +0100

    Smaller batch sizes for CI

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BATCH_SIZE=1000
+BATCH_SIZE=100
 for PROVIDER in eigen ruy mkl blas; do
   echo "+" ./benchmark --batchSize $BATCH_SIZE --provider $PROVIDER
   time ./benchmark --batchSize $BATCH_SIZE --provider $PROVIDER
